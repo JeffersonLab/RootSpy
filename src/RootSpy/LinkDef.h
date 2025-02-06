358ec56ce07a6440b3107590bf118e07ac23a9bf
David Lawrence <davidl@jlab.org>
2025-02-06 09:41:07 -0500

    Add files to build RootSpy GUI with cmake.

---
+++ src/RootSpy/LinkDef.h
@@ -0,0 +1,16 @@
+#ifdef __CINT__
+
+#pragma link C++ class Dialog_ConfigMacros+;
+#pragma link C++ class Dialog_IndivHists+;
+#pragma link C++ class Dialog_NewReference+;
+#pragma link C++ class Dialog_ReferencePlot+;
+#pragma link C++ class Dialog_SaveHists+;
+#pragma link C++ class Dialog_ScaleOpts+;
+#pragma link C++ class Dialog_SelectHists+;
+#pragma link C++ class Dialog_SelectTree+;
+#pragma link C++ class Dialog_ShowGuidance+;
+#pragma link C++ class Dialog_ShowMacro+;
+#pragma link C++ class rs_mainframe+;
+#pragma link C++ class RSTab+;
+
+#endif
